9d01119b9eede0e1a2d86bab0ca9b56ba34b9333
Hunter <thunte0561@greenville.k12.sc.us>
2017-09-18 14:39:09 -0400

    Added play again

--- guess_a_number.py
+++ guess_a_number.py
@@ -15,27 +15,46 @@ def get_guess():
         else:
             print("You must enter a number.")
 
+def play_again():
+    while True:
+        decision = input("You wanna have another go?")
+
+        if decision == 'yes' or decision == 'y' or decision == 'Yes':
+            return True
+        elif decision == 'no' or decision == 'No' or decision == 'n':
+            return False
+        else:
+            print ("Use yes or no you imbecile")
+
+again = True
+
+while again:        
 #start game
-rand = random.randint(low, high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+    rand = random.randint(low, high)
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
-guess = -1
-tries = 0
-limit = 10
+    guess = -1
+    tries = 0
+    limit = 10
 
-#play game
-while guess != rand and tries < limit:
-    guess = get_guess()
-    
-    if guess < rand:
-        print("You guessed too low.")
-    elif guess > rand:
-        print("You guessed too high.")
+    #play game
+    while guess != rand and tries < limit:
+        guess = get_guess()
         
-    tries +=1
+        if guess < rand:
+            print("You guessed too low.")
+        elif guess > rand:
+            print("You guessed too high.")
+            
+        tries +=1
 
-# end
-if guess == rand:
-    print("Good job kid, you know basic math")
-else:
-    print ("You're trash at everything, loser. The number was " + str(rand) + ".")
+    # end
+    if guess == rand:
+        print("Good job kid, you know basic math")
+    else:
+        print ("You're trash at everything, loser. The number was " + str(rand) + ".")
+
+    again = play_again()
+
+
+print("Peace nerd")
