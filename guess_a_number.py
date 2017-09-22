aa0f3e76b4bcb958e5f6c6e0fc6cf3be59a592ec
Hunter <thunte0561@greenville.k12.sc.us>
2017-09-22 14:49:20 -0400

    Final Version, but really tho

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,9 +1,10 @@
 import random
+import math
 
 # config
 low = 1
 high = 100
-limit = 7
+limit = math.ceil(math.log(high-low + 1, 2))
 
 # helper functions
 def show_start_screen():
@@ -18,27 +19,36 @@ def show_start_screen():
 
 def show_credits():
     print("This awesome game was created by Bagel Brandon.")
+    print("")
+
     
 def get_guess():
     while True:
         guess = input("Guess a number: ")
+        print("")
+
 
         if guess.isnumeric():
             guess = int(guess)
             return guess
         else:
             print("You must enter a number.")
+            print("")
+
 
 def pick_number():
-    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +". You have " + str(limit) + " turns to guess correctly.")
+    print("")
 
     return random.randint(low, high)
 
 def check_guess(guess, rand):
     if guess < rand:
         print("You guessed too low.")
+        print("")
     elif guess > rand:
         print("You guessed too high.")
+        print("")
 
 def show_result(guess, rand):
     if guess == rand:
@@ -49,13 +59,15 @@ def show_result(guess, rand):
 def play_again():
     while True:
         decision = input("Would you like to play again? (y/n) ")
+        print("")
 
-        if decision == 'y' or decision == 'yes':
+        if decision == 'y' or decision == 'yes' or decision == 'Yes' or decision == 'YES':
             return True
-        elif decision == 'n' or decision == 'no':
+        elif decision == 'n' or decision == 'no'or decision == 'No' or decision == 'NO':
             return False
         else:
             print("I don't understand. Please enter 'y' or 'n'.")
+            print("")
 
 def play():
     guess = -1
